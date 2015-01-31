e4ad20cb3818c6bc3097c5a6ee9452a18cdeea6b
RussFenenga <russfenenga@gmail.com>
2015-01-31 09:41:46 -0800

    Added Second View Controller
    Toggle between events and the map.
    Custom Map Icon added

---
+++ EventHype/EventHype/eventList.swift
@@ -0,0 +1,13 @@
+//
+//  eventList.swift
+//  EventHype
+//
+//  Created by Russ Fenenga on 1/31/15.
+//  Copyright (c) 2015 SBHacks. All rights reserved.
+//
+
+import UIKit
+
+class eventList: UITableViewController {
+
+}
