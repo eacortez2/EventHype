532aecb4569eeda1ac4c3e4da7ac6ebed10f7381
RussFenenga <russfenenga@gmail.com>
2015-01-30 20:59:55 -0800

    Added Firebase Frameworks
    Firebase functionality added plus a root reference to the view
    controller.

--- EventHype/EventHype/ViewController.swift
+++ EventHype/EventHype/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var myRootRef = Firebase(url:"https://eventhype.firebaseio.com")
 
     override func viewDidLoad() {
         super.viewDidLoad()
