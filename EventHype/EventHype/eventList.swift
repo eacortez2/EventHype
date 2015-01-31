f3575f3b2bf715e92c43fe2856cf9610277852ca
RussFenenga <russfenenga@gmail.com>
2015-01-31 11:14:05 -0800

    Added Event List Class
    -working on setting up table view

--- EventHype/EventHype/eventList.swift
+++ EventHype/EventHype/eventList.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
-class eventList: UITableViewController {
+class eventList: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var eventList: UITableView!
+    
+    
+    override func viewDidLoad() {
+        //we need to pull the data from the firebase chat and figure out how many rows we need
+        
+    }
+    
 
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 2
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 1
+        
+    }
+    
+   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String("test")
+        
+        return cell
+    }
+    
 }
