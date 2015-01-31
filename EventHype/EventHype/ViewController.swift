13ea7de1f7e459c4b4aad29c9eceb11c5fa06221
RussFenenga <russfenenga@gmail.com>
2015-01-30 20:53:40 -0800

    First Commit
    Plz don’t break

---
+++ EventHype/EventHype/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EventHype
+//
+//  Created by Russ Fenenga on 1/30/15.
+//  Copyright (c) 2015 SBHacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
