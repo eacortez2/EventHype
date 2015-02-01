8c5bc595008459e088182aa20cf34ffef3a4b36c
Ashley <eacortez2@pipeline.sbcc.edu>
2015-02-01 01:01:28 -0800

    EventInfoViewController

---
+++ EventHype/EventHype/EventInfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EventInfoViewController.swift
+//  EventHype
+//
+//  Created by turbs on 1/31/15.
+//  Copyright (c) 2015 SBHacks. All rights reserved.
+//
+
+import UIKit
+
+class EventInfoViewControl: UIViewController{
+    
+    var myRootRef = Firebase(url:"https://eventhype.firebaseio.com/events")
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
